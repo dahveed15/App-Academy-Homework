9a37c9cf584f1327586ecbbef9a9d25c7ee1cae7
David Harris <David Harris.dahveed15@gmail.com>
2017-12-09 19:42:57 -0800

    Add W2D1 HW

---
+++ W2D1 HW/skeleton/lib/simon.rb
@@ -0,0 +1,50 @@
+class Simon
+  COLORS = %w(red blue green yellow)
+
+  attr_accessor :sequence_length, :game_over, :seq
+
+  def initialize
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+
+  def play
+    take_turn until game_over == true
+    game_over_message
+    reset_game
+  end
+
+  def take_turn
+    show_sequence
+    require_sequence
+    round_success_message
+    @sequence_length += 1
+  end
+
+  def show_sequence
+    add_random_color
+  end
+
+  def require_sequence
+    
+  end
+
+  def add_random_color
+    colors = %w[red blue green yellow]
+
+    @seq.push(colors.sample)
+  end
+
+  def round_success_message
+    puts 'Congrats! You passed the current round'
+  end
+
+  def game_over_message
+    puts 'Game Over. Play again.'
+  end
+
+  def reset_game
+    initialize
+  end
+end
