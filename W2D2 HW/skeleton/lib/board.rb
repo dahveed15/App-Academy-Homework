87751595a573ee9e94a7914a62a1e892a012ebef
David Harris <David Harris.dahveed15@gmail.com>
2017-12-11 23:10:26 -0800

    Add W2D2 hw

---
+++ W2D2 HW/skeleton/lib/board.rb
@@ -0,0 +1,49 @@
+class Board
+  attr_accessor :cups
+
+  def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2
+    @cups = Array.new(14) { [] }
+    place_stones
+  end
+
+  def place_stones
+    # helper method to #initialize every non-store cup with four stones each
+    @cups.each_with_index do |cup, idx|
+      next if idx == 6 || idx == 13
+      4.times do
+        cup << :stone
+      end
+    end
+  end
+
+  def valid_move?(start_pos)
+    valid_positions = [1..11].to_a
+
+    raise "Invalid starting cup" unless valid_positions.include?(start_pos)
+  end
+
+  def make_move(start_pos, current_player_name)
+      self.cups[start_pos] = []
+
+  end
+
+  def next_turn(ending_cup_idx)
+    # helper method to determine what #make_move returns
+  end
+
+  def render
+    print "      #{@cups[7..12].reverse.map { |cup| cup.count }}      \n"
+    puts "#{@cups[13].count} -------------------------- #{@cups[6].count}"
+    print "      #{@cups.take(6).map { |cup| cup.count }}      \n"
+    puts ""
+    puts ""
+  end
+
+  def one_side_empty?
+  end
+
+  def winner
+  end
+end
