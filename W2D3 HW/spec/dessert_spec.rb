9af499b8cee6bb8f12c4c77e1c05b1573cf77a7f
David Harris <David Harris.dahveed15@gmail.com>
2017-12-13 00:12:51 -0800

    Add W2D3 HW

---
+++ W2D3 HW/spec/dessert_spec.rb
@@ -0,0 +1,68 @@
+require 'rspec'
+require 'dessert'
+
+=begin
+Instructions: implement all of the pending specs (the `it` statements without blocks)! Be sure to look over the solutions when you're done.
+=end
+
+describe Dessert do
+  let(:chef) { double("chef", name: "David") }
+  let(:cookie) { Dessert.new('cookie', 50, chef) }
+
+  describe "#initialize" do
+    it "sets a type" do
+      expect(cookie.type).to eq("cookie")
+    end
+    it "sets a quantity" do
+      expect(cookie.quantity).to eq(50)
+    end
+
+    it "starts ingredients as an empty array" do
+      expect(cookie.ingredients).to be_empty
+    end
+
+    it "raises an argument error when given a non-integer quantity" do
+      expect { Dessert.new('cookie', '#yoloswag', chef) }.to raise_error
+    end
+  end
+
+  describe "#add_ingredient" do
+    it "adds an ingredient to the ingredients array" do
+      cookie.add_ingredient('flour')
+      expect(cookie.ingredients).to include('flour')
+    end
+
+  end
+
+  describe "#mix!" do
+    it "shuffles the ingredient array" do
+      ingredients = ['flour', 'baking soda', 'sugar']
+
+      ingredients.each do |ingredient|
+        cookie.add_ingredient(ingredient)
+      end
+
+      expect(cookie.ingredients).to eq(ingredients)
+      cookie.mix!
+      expect(cookie.ingredients).not_to eq(ingredients)
+      expect(cookie.ingredients.sort).to eq(ingredients.sort)
+    end
+
+  end
+
+  describe "#eat" do
+    it "subtracts an amount from the quantity"
+
+    it "raises an error if the amount is greater than the quantity" do
+      expect { cookie.eat(50) }.to raise_error
+    end
+  end
+
+  describe "#serve" do
+    it "contains the titleized version of the chef's name"
+  end
+
+  describe "#make_more" do
+    it "calls bake on the dessert's chef with the dessert passed in"
+  end
+end
