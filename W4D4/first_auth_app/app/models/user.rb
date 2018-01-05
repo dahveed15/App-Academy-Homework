aec04ae21e355466b8dd170956b46b6320e61cba
David Harris <David Harris.dahveed15@gmail.com>
2018-01-04 20:41:17 -0800

    Add W4D5

--- W4D4/first_auth_app/app/models/user.rb
+++ W4D4/first_auth_app/app/models/user.rb
@@ -10,7 +10,25 @@ class User < ApplicationRecord
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
     return user if user && BCrypt::Password.new(user.password_digest).is_password?(password)
+    nil
   end
 
-  
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
 end
